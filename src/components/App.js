01614f9685a3b3e79f3d7394fbde53c9d10ab3c6
Pere <pere.solaclaver@gmail.com>
2020-11-11 23:04:30 +0000

    Signup component

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SignUp from './Signup';
+import { Container } from 'react-bootstrap';
+
+function App() {
+	return (
+		<Container
+			className='d-flex align-items-center justify-content-center'
+			style={{ minHeight: '100vh' }}
+		>
+			<div className='w-100' style={{ maxWidth: '400px' }}>
+				<SignUp />
+			</div>
+		</Container>
+	);
+}
+
+export default App;
