af6f907775f978ff29d955192a549e54e43bf8d8
Pere <pere.solaclaver@gmail.com>
2020-11-13 23:34:46 +0000

    Login and redirect after login and signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,11 +3,10 @@ import { Form, Card, Button, Alert } from 'react-bootstrap';
 import { useAuth } from '../context/AuthContext';
 import { Link, useHistory } from 'react-router-dom';
 
-export default function Signup() {
+export default function Login() {
 	const emailRef = useRef();
 	const passwordRef = useRef();
-	const passwordConfirmRef = useRef();
-	const { signup } = useAuth();
+	const { login } = useAuth();
 	const [error, setError] = useState('');
 	const [loading, setLoading] = useState(false);
 	const history = useHistory();
@@ -15,18 +14,13 @@ export default function Signup() {
 	async function handleSubmit(e) {
 		e.preventDefault();
 
-		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-			// we return to exit the function and do not signup
-			return setError('Passwords do not match');
-		}
-
 		try {
 			setError('');
 			setLoading(true);
-			await signup(emailRef.current.value, passwordRef.current.value);
+			await login(emailRef.current.value, passwordRef.current.value);
 			history.push('/');
 		} catch {
-			setError('Failed to create an account');
+			setError('Failed to sign in');
 		}
 		setLoading(false);
 	}
@@ -35,7 +29,7 @@ export default function Signup() {
 		<>
 			<Card>
 				<Card.Body>
-					<h2 className='text-center mb-4'>Sign Up</h2>
+					<h2 className='text-center mb-4'>Log In</h2>
 					{error && <Alert variant='danger'>{error}</Alert>}
 					<Form onSubmit={handleSubmit}>
 						<Form.Group id='email'>
@@ -46,18 +40,14 @@ export default function Signup() {
 							<Form.Label>Password</Form.Label>
 							<Form.Control type='password' ref={passwordRef} required />
 						</Form.Group>
-						<Form.Group id='password-confirm'>
-							<Form.Label>Password Confirmation</Form.Label>
-							<Form.Control type='password' ref={passwordConfirmRef} required />
-						</Form.Group>
 						<Button disabled={loading} type='submit' className='w-100'>
-							Sign Up
+							Log In
 						</Button>
 					</Form>
 				</Card.Body>
 			</Card>
 			<div className='w-100 text-center mt-2'>
-				Already have an account? <Link to='/login'>Log in</Link>
+				Need an account? <Link to='/signup'>Sign Up</Link>
 			</div>
 		</>
 	);
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -14,6 +14,11 @@ export function AuthProvider({ children }) {
 	function signup(email, password) {
 		return auth.createUserWithEmailAndPassword(email, password);
 	}
+
+	function login(email, password) {
+		return auth.signInWithEmailAndPassword(email, password);
+	}
+
 	useEffect(() => {
 		const unsubscribe = auth.onAuthStateChanged((user) => {
 			setCurrentUser(user);
@@ -26,7 +31,12 @@ export function AuthProvider({ children }) {
 	const value = {
 		currentUser,
 		signup,
+		login,
 	};
 
-	return (<AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>);
+	return (
+		<AuthContext.Provider value={value}>
+			{!loading && children}
+		</AuthContext.Provider>
+	);
 }
